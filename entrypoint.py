f36645dc543081f69e44a503a000a43fe2d037fe
emil <Emil.Aminov@tatar.ru>
2021-03-14 17:03:15 +0300

    :bug: fix bug with current file

--- entrypoint.py
+++ entrypoint.py
@@ -6,14 +6,15 @@ if __name__ == '__main__':
     app_description = input('Write your app description: ')
     author = input('Login of author of this repository: ')
     for root, dirs, files in os.walk('.'):
+        print(files)
         for file in files:
-            if '.git' or '.idea' in root:
+            if '.git' or '.idea' in root or __file__ not in file:
                 continue
             print(root)
             print(os.path.join(root, file))
             with open(os.path.join(root, file), 'r') as f:
                 content = f.read()
-            new_content = content.replace('1', app_name).replace('2', app_slug).replace(
-                '3', app_description).replace('4', author)
+            new_content = content.replace('__app_name__', app_name).replace('__app_slug__', app_slug).replace(
+                '__app_description__', app_description).replace('__author__', author)
             with open(os.path.join(root, file), 'w') as f:
                 f.write(new_content)
