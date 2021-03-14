332816313c971ec08695b89f49ded5bc7abaa277
emil <Emil.Aminov@tatar.ru>
2021-03-14 16:57:31 +0300

    :sparkles: Add script for generating docs

--- entrypoint.py
+++ entrypoint.py
@@ -2,14 +2,18 @@ import os
 
 if __name__ == '__main__':
     app_name = input('Write your app name: ')
-    app_slug = input('Write your app slug: ')
-    app_description = input('Write your app description: ')
+    app_slug = input('Write your 2: ')
+    app_description = input('Write your 3ption: ')
     author = input('Login of author of this repository: ')
     for root, dirs, files in os.walk('.'):
         for file in files:
-            with open(file, 'r') as f:
+            if '.git' or '.idea' in root:
+                continue
+            print(root)
+            print(os.path.join(root, file))
+            with open(os.path.join(root, file), 'r') as f:
                 content = f.read()
-            new_content = content.replace('__app_name__', app_name).replace('__app_slug__', app_slug).replace(
-                '__app_description__', app_description).replace('__author__', author)
-            with open(file, 'w') as f:
+            new_content = content.replace('1', app_name).replace('2', app_slug).replace(
+                '3', app_description).replace('4', author)
+            with open(os.path.join(root, file), 'w') as f:
                 f.write(new_content)
